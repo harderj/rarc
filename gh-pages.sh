3c252c71bd58a9c9d60140d65a6883c782cb1742
Jacob Harder <harderjacob@gmail.com>
2025-05-17 00:11:44 +0200

    add deploy script

---
+++ gh-pages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cargo install wasm-bindgen-cli
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/rarc.wasm
